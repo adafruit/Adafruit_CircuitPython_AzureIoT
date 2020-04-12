f683715cc6e701b60187ebabff2e1b60e2e978f4
Jim Bennett <jim.bennett@microsoft.com>
2020-04-12 16:25:22 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ extensions = [
 # Uncomment the below if you use native CircuitPython modules such as
 # digitalio, micropython and busio. List the modules you use. Without it, the
 # autodoc module docs will fail to generate with a warning.
-autodoc_mock_imports = ["adafruit_logging", "device_registration", "iot_mqtt", "iot_error"]
+autodoc_mock_imports = ["adafruit_logging", "adafruit_requests", "device_registration", "iot_mqtt", "iot_error"]
 
 
 intersphinx_mapping = {
