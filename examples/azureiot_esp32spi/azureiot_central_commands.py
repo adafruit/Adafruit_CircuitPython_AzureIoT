028300fcb46a7dda55a3184e69019b940de81c0d
Justin Myers <justmobilize@users.noreply.github.com>
2025-02-24 13:54:22 -0800

    Remove secrets usage

--- examples/azureiot_esp32spi/azureiot_central_commands.py
+++ examples/azureiot_esp32spi/azureiot_central_commands.py
@@ -1,6 +1,7 @@
 # SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
 # SPDX-License-Identifier: MIT
 
+from os import getenv
 import time
 import board
 import busio
@@ -10,12 +11,12 @@ import rtc
 from adafruit_esp32spi import adafruit_esp32spi, adafruit_esp32spi_wifimanager
 import adafruit_connection_manager
 
-# Get wifi details and more from a secrets.py file
-try:
-    from secrets import secrets
-except ImportError:
-    print("WiFi secrets are kept in secrets.py, please add them there!")
-    raise
+# Get WiFi details and AWS Keys, ensure these are setup in settings.toml
+ssid = getenv("CIRCUITPY_WIFI_SSID")
+password = getenv("CIRCUITPY_WIFI_PASSWORD")
+id_scope = getenv("id_scope")
+device_id = getenv("device_id")
+device_sas_key = getenv("device_sas_key")
 
 # ESP32 Setup
 try:
@@ -31,19 +32,21 @@ spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
 esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
 
 """Use below for Most Boards"""
-status_light = neopixel.NeoPixel(
+status_pixel = neopixel.NeoPixel(
     board.NEOPIXEL, 1, brightness=0.2
 )  # Uncomment for Most Boards
 """Uncomment below for ItsyBitsy M4"""
-# status_light = dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1, brightness=0.2)
+# status_pixel = dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1, brightness=0.2)
 # Uncomment below for an externally defined RGB LED
 # import adafruit_rgbled
 # from adafruit_esp32spi import PWMOut
 # RED_LED = PWMOut.PWMOut(esp, 26)
 # GREEN_LED = PWMOut.PWMOut(esp, 27)
 # BLUE_LED = PWMOut.PWMOut(esp, 25)
-# status_light = adafruit_rgbled.RGBLED(RED_LED, BLUE_LED, GREEN_LED)
-wifi = adafruit_esp32spi_wifimanager.ESPSPI_WiFiManager(esp, secrets, status_light)
+# status_pixel = adafruit_rgbled.RGBLED(RED_LED, BLUE_LED, GREEN_LED)
+wifi = adafruit_esp32spi_wifimanager.WiFiManager(
+    esp, ssid, password, status_pixel=status_pixel
+)
 
 print("Connecting to WiFi...")
 
@@ -86,7 +89,7 @@ print("Time:", str(time.time()))
 #
 # Next create a device using the device template, and select Connect to get the device connection
 # details.
-# Add the connection details to your secrets.py file, using the following values:
+# Add the connection details to your settings.toml file, using the following values:
 #
 # 'id_scope' - the devices ID scope
 # 'device_id' - the devices device id
@@ -108,9 +111,9 @@ ssl_context = adafruit_connection_manager.get_radio_ssl_context(esp)
 device = IoTCentralDevice(
     pool,
     ssl_context,
-    secrets["id_scope"],
-    secrets["device_id"],
-    secrets["device_sas_key"],
+    id_scope,
+    device_id,
+    device_sas_key,
 )
 
 
